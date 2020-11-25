ffd2b4892d357f8e2438b69ae85cfae69b911114
Mousaidna Rosario <74958156+mousaidnarosario@users.noreply.github.com>
2020-11-26 10:09:57 +1100

    Update Predictive analysis using Linear model for house pricing dataset.R

--- Predictive analysis using Linear model for house pricing dataset.R
+++ Predictive analysis using Linear model for house pricing dataset.R
@@ -1,8 +1,8 @@
 ###
-# Title: Predicting house pricing using linear regression model
-# Problem Statement: Predict the house pricing using Linear Regression
+# Title: Explore linear regression model in predicting data
+# Problem Statement: Predict the USA house prices using Linear Regression
 # Author: "Mousaidna Rosario"
-# Dataset description : Dataset from Kaggle.com called Housing prices dataset
+# Dataset description : Dataset from studygyaan.com called usa housing prices dataset
 # Predictive analytics methods:
 # Import data > Data cleaning > build a model > Train model > Test model > Improve efficiency
 ###
@@ -10,32 +10,41 @@
 
 # Step 1. Import data 
 
-pricing <- read.csv("housingdataset.csv")
+usa.pricing <- read.csv("usaHousing.csv")
 
-head(pricing) # show the first 5 data of the data
-dim(pricing)  # summary dimension of data,(rows, columns)
-summary(pricing) # quick view of how the data looks, according to mean, median, min and max
+head(usa.pricing) # show the first 5 data of the data
+dim(usa.pricing)  # summary dimension of data,(rows, columns)
+summary(usa.pricing) # quick view of how the data looks, according to mean, median, min and max
 
 
-# STEP 2 . Data cleaning
+# sTEP 2 . Data cleaning
 
 # Display structure of data
-str(pricing)
+str(usa.pricing)
+
+# fix the label of the data
+names(usa.pricing) <- c("Avg.Area.Income", "Avg.House.Age","Avg.Rooms" ,"Avg.Area.Bedrooms",
+                        "Population","Price","Address")
+
+
+str(usa.pricing)
 
 # Pull important columns from the dataset and put it to another table for ease of use. 
-# The dataset is big with 81 columns and it gets harder to process if we don't remove unnecessary data in the model.
+# I will not need address for this model
 
-house.pricing <-pricing[,c("LotArea","YearBuilt","BedroomAbvGr","SalePrice")]
+uhp <-usa.pricing[,c("Avg.Area.Income", "Avg.House.Age","Avg.Rooms" ,"Avg.Area.Bedrooms",
+                           "Population","Price")]
 
-str(house.pricing) # from 81 columns to 4 important variables to the Problem
-summary(house.pricing)
-plot(house.pricing )  # initial plot, will show you if the data will fit a linear model. 
+str(uhp) # I will use this df moving forward
+summary(uhp)
+plot(uhp)  # initial plot, will show you if the data will fit a linear model. 
 
 par(mfrow=c(2,2))
 
-plot(SalePrice ~ YearBuilt , data=house.pricing )
-plot(SalePrice ~ BedroomAbvGr , data=house.pricing )
-plot(SalePrice ~ LotArea  , data=house.pricing )
+# initial look at different things to train on to know the price.
+plot(Price ~ Avg.Area.Income , data=uhp )
+plot(Price ~ Avg.Rooms , data=uhp )
+plot(Price ~ Avg.House.Age , data=uhp )
 
 # Data preparation
 
@@ -43,49 +52,71 @@ plot(SalePrice ~ LotArea  , data=house.pricing )
 # Pull data that has no missing values
 # Missing values can cause inaccurate data
 
-house.pricing <- house.pricing[complete.cases(house.pricing),]
+uhp <- uhp[complete.cases(uhp),]
 
-str(house.pricing) # no data is removed because all variables have values. 
+str(uhp) # no data is removed because all variables have values. 
 
 # STEP 3. Build the model
 
 set.seed(2)
-install.packages("caTools") # splitting and predicting data
+#install.packages("caTools") # splitting data
 library(caTools)
 
 # Split data into training and testing set. Training is always bigger than testing set 
-n <- nrow(house.pricing)
+n <- nrow(uhp)
 s <- n *0.8 # 80% : data from 1:s
 s1 <- s+1   # 20% : s+1 to the end of dataset
 
-training.Set <- house.pricing[1:s,1:4]
-testing.Set <- house.pricing[s1:n,1:4]
+training.Set <- uhp[1:s,1:6]
+testing.Set <- uhp[s1:n,1:6]
 
 nrow(training.Set)
 nrow(testing.Set)
 
+head(training.Set)
+head(testing.Set)
+
 # STEP 4 Train the model 
-model <- lm(SalePrice ~ ., data = training.Set )
+model <- lm(Price ~ ., data = training.Set ) # dot shows multiple linear regression model
 
 summary(model)
 
 # Prediction where we use the testing data
 
 predict.model <- predict(model, testing.Set) # predict using testing set. part of catools package
-predict.model
+head(predict.model)
 
 # STEP 5. TEST THE MODEL
 # Test outcomes by comparing predicted data vs observed data
-plot(testing.Set$SalePrice, lty = 2, col = 2, type="l")
+
+par(mfrow=c(2,2))
+plot(testing.Set$Price, lty = 2, col = 2, type="l", main="Predicted and testing model overlap")
 lines(predict.model, type="l",col=4) # plot overlap testing
-plot(predict.model, lty = 2, col = 4, type="l")
+plot(predict.model, lty = 2, col = 4, type="l", main="Predicted model")
+
+# the plot shows that they frequency of testing and predicted model is almost the same, (predicted model covers test data well)
 
 # STEP 6. Improve efficiency
 
+hist(testing.Set$Price-predict.model) # the  model and testing set shows normal distribution which is good.
+
+plot(testing.Set$Price,predict.model, main="Predicted data vs testing data\n in LM model", col=c(2,3))
+      # the model and testing set is linear 
+legend("bottomright", "(Predicted, Testing)", pch=1, title= "bottomright")
+     
+     
 # Find accuracy
 head(predict.model)
-head(house.pricing$SalePrice)
-rmse <- sqrt(mean(house.pricing$SalePrice-predict.model)^2) # root difference error
+head(uhp$Price)
+mse <- mean((uhp$Price -predict.model)^2) 
+rmse = sqrt(mse)
 rmse
-# Using RMSE (regression model accuracy measurement),
-#371.4961 means The next time I will use this model to predict sales, I am off around $371.4961. 
+
+#or install caret for testing 
+
+#install.packages('caret')
+library(caret)
+
+RMSE(predict.model,uhp$Price)
+R2(predict.model,uhp$Price, form = "traditional")
+
